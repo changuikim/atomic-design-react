dbe9d2f8de28acd00c5c7601be525dfb3f44964b
changuikim <cu.kim.dev@gmail.com>
2024-06-12 23:58:13 +0900

    [Feat] 아토믹 디자인으로 블로그 앱의 템플릿/페이지 컴포넌트를 구현
    - BlogList, BlogListPage

---
+++ src/blogApp/pages/BlogListPage/index.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+import BlogList from '@/blogApp/components/templates/BlogList'
+import type { Post } from '@/blogApp/components/templates/BlogList'
+
+const BlogListPage: React.FC = () => {
+  const [posts, setPosts] = useState<ReadonlyArray<Post>>([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => setPosts(json))
+      .catch(error => {
+        throw new Error(error)
+      })
+  }, [])
+
+  return <BlogList posts={posts} />
+}
+
+export default BlogListPage
